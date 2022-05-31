4581ea97a4d2e66d10025fd0f317a5b75b776631
Anh Huynh <huynhanh8327@gmail.com>
2022-05-31 12:57:34 -0700

    progress on landing page

--- routes.jl
+++ routes.jl
@@ -1,9 +1,28 @@
-using Genie.Router, Stipple
+using Genie.Router, Stipple, StippleUI
 
 route("/") do
 	serve_static_file("index.html")
 end
 
+@reactive mutable struct Name <: ReactiveModel
+	name::R{String} = "World!"
+end
+  
+function ui(model)
+	page( model, class="container", [
+		h1([
+			"Hello "
+			span("", @text(:name))
+		])
+  
+		p([
+			"What is your name? "
+			input("", placeholder="Type your name", @bind(:name))
+		])
+	])
+end
+
 route("/hello") do
-	nothing
+	model = Name |> init
+  	html(ui(model), context = @__MODULE__)
 end
